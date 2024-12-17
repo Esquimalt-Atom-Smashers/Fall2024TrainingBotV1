f9e8bd56f5ca48eac0166295d64a6697f0314972
21stcenturymacgyver <21stcenturymacgyver@gmail.com>
2024-12-16 18:04:51 -0800

    flipped to default as field centric

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -44,7 +44,7 @@ public class RobotContainer {
                 () -> MathUtil.applyDeadband(-driver.getRawAxis(translationAxis), Constants.Teleop.JOYSTICK_DEADBAND)*Constants.Teleop.MAX_SPEED_M_S,
                 () -> MathUtil.applyDeadband(driver.getRawAxis(strafeAxis), Constants.Teleop.JOYSTICK_DEADBAND)*Constants.Teleop.MAX_SPEED_M_S,
                 () -> MathUtil.applyDeadband(driver.getRawAxis(rotationAxis), Constants.Teleop.JOYSTICK_DEADBAND)*Constants.Teleop.MAX_ANG_SPEED_RAD_S,
-                () -> robotCentric.getAsBoolean()
+                () -> !robotCentric.getAsBoolean()
                
             )
             
